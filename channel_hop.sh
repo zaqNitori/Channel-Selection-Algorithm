89850f1f72f1bdf3a4f9deac1f22715d0ddfe045
zaqNitori <zaq81009@gmail.com>
2023-05-23 20:51:30 +0800

    Upload channel_hop.sh first

---
+++ channel_hop.sh
@@ -0,0 +1,32 @@
+#!/bin/ash
+
+channel_24="1 2 3 4 5 6 7 8 9 10 11"
+
+if [[ $# -eq 0 ]]
+    then
+        echo "Please give interface!"
+        exit 0
+fi
+interface="${1}"
+
+ifconfig wlan0 down
+ifconfig wlan0-2 down
+
+awk -f frame_scan.awk
+
+for ch in ${channel_24}
+do
+    echo "${interface} Setting channel ${ch}"
+    iw dev "${interface}" set channel "${ch}"
+    sleep 1
+done
+
+pkill tcpdump
+
+echo "Channel Hopping Done!"
+iw dev "${interface}" set channel 6
+ifconfig wlan0 up
+ifconfig wlan0-2 up
+sleep 1
+
+wifi
