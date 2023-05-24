b3c2eea3e9ef4bd07c2d724d01c2780c77c63966
zaqNitori <zaq81009@gmail.com>
2023-05-24 17:06:24 +0800

    Add option i for interface s for sleep_interval f for for_time

--- channel_hop.sh
+++ channel_hop.sh
@@ -1,27 +1,42 @@
 #!/bin/ash
 
-channel_24="1 2 3 4 5 6 7 8 9 10 11 12 13 14"
+channel_24="1 2 3 4 5 6 7 8 9 10 11 12 13"
+interface=""
+sleep_interval=1
+for_time=1
 
-if [[ $# -eq 0 ]]
-    then
-        echo "Please give interface!"
-        exit 0
+while getopts i:s:f: flag
+do
+    case "${flag}" in
+        i) interface=${OPTARG};;
+        s) sleep_interval=${OPTARG};;
+        f) for_time=${OPTARG};;
+    esac
+done
+
+if [ "${interface}" == "" ]; then
+    echo "Please give interface!"
+    exit 0
 fi
-interface="${1}"
 
 ifconfig wlan0 down
 ifconfig wlan0-2 down
+iw dev "${interface}" set channel 14
 
-for ch in ${channel_24}
+for i in $(seq 1 1 ${for_time})
 do
-    echo "${interface} Setting channel ${ch}"
-    iw dev "${interface}" set channel "${ch}"
-    sleep 1
+    for ch in ${channel_24}
+    do
+        echo "${interface} Setting channel ${ch}"
+        iw dev "${interface}" set channel "${ch}"
+        sleep "${sleep_interval}"
+    done
 done
 
 pkill tcpdump
 
-echo "\nChannel Hopping Done!"
+echo ""
+echo "Channel Hopping Done!"
 iw dev "${interface}" set channel 6
 ifconfig wlan0 up
 ifconfig wlan0-2 up
