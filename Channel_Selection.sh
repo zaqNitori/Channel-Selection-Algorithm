784c11f44207068a2715e6ebaf0c0b06322ed79a
zaqNitori <zaq81009@gmail.com>
2023-05-30 21:11:26 +0800

    move getopt part and some preprocessing part from channel_hop.sh to here. And will call channel_hop.sh and frame_scan.awk together and that them execute in background

--- Channel_Selection.sh
+++ Channel_Selection.sh
@@ -5,8 +5,39 @@
 # and combine them to show a more precisely channel measurement.
 #
 
-# Will Create an empty file and call iwchan.awk to store the output data
-touch result & awk -f iwchan.awk show phy0 > result
+# Get output from iwchan and store it.
+effect=`awk -f iwchan.awk show phy0`
+echo "${effect}"
 
 # Then will call frame scan
+
+interface=""
+si=1
+ft=1
+
+while getopts i:s:f: flag
+do
+    case "${flag}" in
+        i) interface=${OPTARG};;
+        s) si=${OPTARG};;
+        f) ft=${OPTARG};;
+    esac
+done
+
+if [ "${interface}" == "" ]; then
+    echo "Please give interface!"
+    exit 0
+fi
+
+ifconfig wlan0 down
+ifconfig wlan0-2 down
+iw dev "${interface}" set channel 14
+
+./channel_hop.sh "${ft}" "${si}" "${interface}" & awk -f frame_scan.awk "${interface}"
+
+wait
+echo "CS Finish!!"
+
+
+
 
