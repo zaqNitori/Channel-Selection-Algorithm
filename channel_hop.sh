93b76092da3da01393a9ca47ed7cbaeeebf7f9c2
zaqNitori <zaq81009@gmail.com>
2023-05-30 21:19:39 +0800

    Fix calling variables. Can use input from cmd now

--- channel_hop.sh
+++ channel_hop.sh
@@ -3,16 +3,16 @@
 channel_24="1 2 3 4 5 6 7 8 9 10 11 12 13"
 ft=$1
 si=$2
-interface=$3
+itf=$3
 
 sleep 1
 
-for i in $(seq 1 1 1)
+for i in $(seq 1 1 $ft)
 do
     for ch in ${channel_24}
     do
-        iw dev wlan0-1 set channel "${ch}"
-        sleep 1
+        iw dev "${itf}" set channel "${ch}"
+        sleep $si
     done
 done
 
@@ -22,7 +22,7 @@ pkill "tcpdump"
 
 echo ""
 echo "Channel Hopping Done!"
-iw dev "${interface}" set channel 6
+iw dev "${itf}" set channel 6
 ifconfig wlan0 up
 ifconfig wlan0-2 up
 sleep 1
