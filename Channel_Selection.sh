efcb2847405c01a2a3b2f0f3787b30ce8fbde758
zaqNitori <zaq81009@gmail.com>
2023-06-02 16:44:35 +0800

    Move interface setting from channel_hop.sh to here. And will write another shell script to deal with these

--- Channel_Selection.sh
+++ Channel_Selection.sh
@@ -28,14 +28,25 @@ if [ "${interface}" == "" ]; then
     exit 0
 fi
 
+# Set same phy interface down so we can easily switch channel 
 ifconfig wlan0 down
 ifconfig wlan0-2 down
 iw dev "${interface}" set channel 14
 
+# Call channel_hop and frame_scan to capture frame amount
 ./channel_hop.sh "${ft}" "${si}" "${interface}" & amount=`awk -f frame_scan.awk "${interface}"`
-
 wait
 
+# After frame_scan then set config back for normal use
+iw dev "${itf}" set channel 6
+ifconfig wlan0 up
+ifconfig wlan0-2 up
+sleep 1
+
+# Let the services reload the config
+wifi
+
+# Call another awk script to format the data
 awk -f Combine.awk "${effect}" "${amount}"
 
 echo "CS Finish!!"
