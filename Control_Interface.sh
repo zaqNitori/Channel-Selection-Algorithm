08a268c1a9fd8b3220a5e2b3b747b2d477a528ac
zaqNitori <zaq81009@gmail.com>
2023-06-05 18:24:18 +0800

    Add Interface UP / DOWN Function. now can pass monitor and non-monitor interfaces into this awk and let it do the up and down switch

--- Control_Interface.sh
+++ Control_Interface.sh
@@ -10,26 +10,62 @@ function show_help() {
     echo ""
 }
 
-limit=1
-action=""
+function IsValid() {
 
-if [ $# -ne $limit ] 
+    if [ $1 == "d" ] 
+    then
+        echo "Interface down action mode!"
+    elif [ $1 == "u" ] 
+    then 
+        echo "Interface up action mode!"
+    else 
+        echo "Wrong option!"
+        show_help
+        exit 0
+    fi
+}
+
+IsValid "$1"
+
+function Set_Interface_Up() {
+    
+    for itf in $2
+    do
+        iw dev ${itf} set channel $3
+        break
+    done
+
+    for itf in $1
+    do
+        ifconfig ${itf} up
+    done
+
+    sleep 1
+    wifi
+}
+
+function Set_Interface_Down() {
+
+    for itf in $1
+    do
+        ifconfig ${itf} down
+    done
+
+    for itf in $2
+    do
+        iw dev ${itf} set channel 14
+        break
+    done
+}
+
+non_monitor=$2
+monitor=$3
+chan=$4
+
+if [ $1 == "u" ]
 then
-    echo "Option Syntax wrong!"
-    show_help
-    exit 0
+Set_Interface_Up "${non_monitor}" "${monitor}" "${chan}"
+else
+Set_Interface_Down "${non_monitor}" "${monitor}" "${chan}"
 fi
 
-action=$1
-
-if [ "${action}" == "d" ] 
-then
-    echo "Interface down action mode!"
-elif [ "${action}" == "u" ] 
-then 
-    echo "Interface up action mode!"
-else 
-    echo "Wrong option!"
-    show_help
-    exit 0
-fi
