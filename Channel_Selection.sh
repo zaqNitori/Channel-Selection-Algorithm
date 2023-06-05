bbc91a09c71e0039f01a8d6c7d68b3a46c8f937e
zaqNitori <zaq81009@gmail.com>
2023-06-05 21:21:38 +0800

    Add comments, remove unneeded code and add waiting_chan variables which will pass into Control_Interface

--- Channel_Selection.sh
+++ Channel_Selection.sh
@@ -5,44 +5,53 @@
 # and combine them to show a more precisely channel measurement.
 #
 
-interface=""
+# Variable Setting
 phy=""
 si=1
 ft=1
 
+# Get Setting From Option Arguments
 while getopts i:s:f:p: flag
 do
     case "${flag}" in
         p) phy=${OPTARG};;
-        i) interface=${OPTARG};;
         s) si=${OPTARG};;
         f) ft=${OPTARG};;
     esac
 done
 
+# The p tag phy should be needed
 if [ "${phy}" == "" ]; then
     echo "Please give phy!"
     exit 0
 fi
 
-# Get output from iwchan and store it.
+# Call iwchan.awk first and then store its output
 effect=`awk -f iwchan.awk show ${phy}`
 
-
+# Get interfaces from specific phy
 itf_conf=`awk -f Search_Interface.awk ${phy}`
+
+# use awk to extract the string 
 non_monitor=`echo ${itf_conf} | awk '{split($0, s, "!"); print s[1]}'`
 monitor=`echo ${itf_conf} | awk '{split($0, s, "!"); print s[2]}'`
 original_chan=`echo ${itf_conf} | awk '{split($0, s, "!"); print s[3]}'`
+waiting_chan=`echo ${itf_conf} | awk '{split($0, s, "!"); print s[4]}'`
 
-./Control_Interface.sh d "${non_monitor}" "${monitor}" 14
+# Use the data above to down interfaces
+./Control_Interface.sh d "${non_monitor}" "${monitor}" "${waiting_chan}"
 
+# Call frame_scan and channel_hop to capture the frames and will wait until both finish
 ./channel_hop.sh "${ft}" "${si}" "${monitor}" "${phy}" & amount=`awk -f frame_scan.awk "${phy}" "${monitor}"`
 wait
 
+# Resume Interfaces Settings after capturing the frames
 ./Control_Interface.sh u "${non_monitor}" "${monitor}" "${original_chan}"
 
+# Call another awk script to combine the effect and amount data
 awk -f Combine.awk "${effect}" "${amount}"
 
+# Announce that this script is finished
 echo "CS Finish!!"
 
 
