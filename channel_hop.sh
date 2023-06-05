71eec51bda76f6d092756f1dc4f624c0dfd572f6
zaqNitori <zaq81009@gmail.com>
2023-06-05 21:33:04 +0800

    Adding comments

--- channel_hop.sh
+++ channel_hop.sh
@@ -1,14 +1,17 @@
 #!/bin/ash
 
+# Get input
 ft=$1
 si=$2
 itf=$3
 phy=$4
 
+# Get Channel List
 chan=`awk -f Get_Channel.awk "${phy}"`
 
 sleep 1
 
+# Start Channel hop
 for i in $(seq 1 1 $ft)
 do
     for ch in ${chan}
@@ -19,5 +22,6 @@ do
     done
 done
 
+# Stop tcpdump capturing
 pkill "tcpdump"
 
