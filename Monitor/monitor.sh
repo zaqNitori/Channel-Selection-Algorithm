2a36c147b11c0451d7f43633630962e47deef459
zaqNitori <zaq81009@gmail.com>
2024-03-04 17:48:18 +0800

    Add monitor.sh. This script will control and collect the result from monitor.awk

--- Monitor/monitor.sh
+++ Monitor/monitor.sh
@@ -1,1 +1,40 @@
 #!/bin/ash
+#
+# 在還沒進行 Channel_Selection時不斷地 monitor當前的 channel，並且用 AVG計算(或是網工課，拿來計算 delay還什麼的那ˋ些方法)
+# 當 Channel_Selection結束時，則跟掃出來的結果做計算，就能取得我們的系統地消耗大概佔比多少，
+# 以此讓頻道之間的比較不會因為我們系統的負擔造成錯誤的比對。
+#
+#
+
+check=`tcpdump --version`
+res=$?
+
+if [ $res -ne 0 ]; then
+    echo "Please install tcpdump!"
+    exit 0
+fi
+
+cd ~/cs
+logFile="logMonitor"
+echo "----------monitor.sh----------" >> "${logFile}"
+
+itf=""
+si=1
+while getopts i:s: flag
+do
+    case "${flag}" in
+        i) itf=${OPTARG};;
+        s) si=${OPTARG};;
+    esac
+done
+
+# The p tag phy should be needed
+if [ "${itf}" == "" ]; then
+    echo "Please give monitor interface!"
+    exit 0
+fi
+
+
+
+
+
