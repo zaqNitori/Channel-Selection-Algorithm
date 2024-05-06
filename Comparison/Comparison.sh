c1b617d392a531c2d4d460be3456aea103fdb552
zaqNitori <zaq81009@gmail.com>
2024-05-06 20:29:27 +0800

    Create Comparison.sh file. This file is the root of whole comparison process.

---
+++ Comparison/Comparison.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+#
+# This script is part three of the whole CS process
+# And will compare current working channel with other channels
+#
+
+# Get Input
+phy=$1
+data=$2
+
+chan=`awk -f Get_Current_Channel.awk ${phy}`
+
+# Split and Comapre
+
+
