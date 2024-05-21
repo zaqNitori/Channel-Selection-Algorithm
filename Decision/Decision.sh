77e0c4fa377170cb3d2b974984f2481257779d7c
zaqNitori <zaq81009@gmail.com>
2024-05-21 20:42:10 +0800

    Calculate improvement to decide should we switch channel or not

--- Decision/Decision.sh
+++ Decision/Decision.sh
@@ -1,6 +1,18 @@
-#!bin/ash
+#!/bin/ash
 #
 # Get Target Channel and joule from input
 # And calculate if the diff of joule exceed threshhold
 # If so then switch channel
 #
+
+# Get Input
+data=$1
+chan=$2
+
+# Split and Decide
+switch=`awk -f Decision.awk "${data}" $chan`
+
+# Output decision result
+echo ${switch}
+
+
