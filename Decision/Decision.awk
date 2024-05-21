73f1a50ddcd0a09043df369e6371601e51435830
zaqNitori <zaq81009@gmail.com>
2024-05-21 18:26:52 +0800

    Create Decision directory, and also create shell script and awk script

---
+++ Decision/Decision.awk
@@ -0,0 +1,9 @@
+#!/usr/bin/awk -f
+#
+# Calculate the diff of joule for current channel and target channel
+#
+
+
+
+
+
---
+++ Decision/Decision.sh
@@ -0,0 +1,6 @@
+#!bin/ash
+#
+# Get Target Channel and joule from input
+# And calculate if the diff of joule exceed threshhold
+# If so then switch channel
+#
